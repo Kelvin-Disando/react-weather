0c53871cf1bc55731a23ed3ca282e50ab2a75520
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-03-29 00:20:43 +0300

    add background

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,16 +1,27 @@
 import React , { useEffect }from 'react';
+import classnames from 'classnames'
 import { useDispatch, useSelector } from 'react-redux';
 import {initWeatherApp} from '../../features/configuration'
 import Preloader from '../Preloader'
+import styles from './styles.module.scss'
 
 function Layout({children}) {
+
   const dispatch = useDispatch();
+  const backgroundImg = useSelector(state => state.configuration.app.background)
+  const additionStyles = {
+    backgroundImage: `url(${backgroundImg})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat'
+  }
   const loadingState = useSelector(state => state.configuration.firstLaunch)
   useEffect(() => {
     dispatch(initWeatherApp())
   }, [])
+  debugger
   return (
-    <div className='wrapper'>
+    <div className={styles.wrapper} style={additionStyles}>
     {loadingState && <Preloader/>}
     {!loadingState && children}
     </div>
