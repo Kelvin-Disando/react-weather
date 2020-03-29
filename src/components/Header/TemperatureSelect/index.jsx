6db4c48eba7af7392501f139883de3756d124439
Dzianis Danilkou <d.danilkou@godeltech.com>
2020-03-29 12:49:49 +0300

    feat: add degrees highlighting

--- src/components/Header/TemperatureSelect/index.jsx
+++ src/components/Header/TemperatureSelect/index.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styles from './styles.module.scss';
-import { changeDegreesType } from '../../../features/configuration';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./styles.module.scss";
+import classnames from "classnames";
+import { changeDegreesType } from "../../../features/configuration";
+import { useDispatch, useSelector } from "react-redux";
 
 const TemperatureSelect = () => {
-  const dispatch = useDispatch()
-    const changeDegreesHandler = (type) => {dispatch(changeDegreesType(type))}
-    return (
-      <>
-       <button onClick={() => changeDegreesHandler('c')} className={styles['celsius']} value='c'>&deg;C</button>
-    <button onClick={() => changeDegreesHandler('f')} className={styles['fahrenheit']} value='f'>&deg;F</button>
-      </>
-    )
-}
+  const dispatch = useDispatch();
+  const degreesType = useSelector(state => state.configuration.degreesType);
+  const changeDegreesHandler = type => {
+    dispatch(changeDegreesType(type));
+  };
+  return (
+    <>
+      <button
+        onClick={() => changeDegreesHandler("c")}
+        className={classnames(styles["celsius"], {[`${styles.degrees_unit_active}`]: degreesType === 'c'})}
+        value="c"
+      >
+        &deg;C
+      </button>
+      <button
+        onClick={() => changeDegreesHandler("f")}
+        className={classnames(styles["fahrenheit"], {[`${styles.degrees_unit_active}`]: degreesType === 'f'})}
+        value="f"
+      >
+        &deg;F
+      </button>
+    </>
+  );
+};
 
-export default TemperatureSelect
+export default TemperatureSelect;
